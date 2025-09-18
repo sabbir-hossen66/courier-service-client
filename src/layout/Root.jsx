1d080e3a211f7527e42114404c239fca604b4dd1
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-09-18 22:45:15 +0600

    route setup

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../pages/shared/Navbar';
 
 const Root = () => {
   return (
     <div>
-      <h2>ssss</h2>
+      <Navbar/>
       <Outlet/>
     </div>
   );
---
+++ src/pages/shared/Navbar.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { Link } from "react-router";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navLinks = [
+    { name: "Home", to: "/" },
+    { name: "About", to: "/about" },
+    { name: "Services", to: "/services" },
+    { name: "Contact", to: "/contact" },
+  ];
+
+  return (
+    <nav className="bg-primary shadow-md fixed top-0 left-0 w-full z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          
+          {/* Logo */}
+          <div className="text-2xl font-bold text-gray-200">Fast Courier</div>
+
+          {/* Desktop Menu */}
+          <div className="hidden md:flex space-x-6">
+            {navLinks.map((link) => (
+              <Link
+                key={link.name}
+                to={link.to}
+                className="text-white text-hover transition"
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+
+          {/* Mobile Menu Button */}
+          <div className="md:hidden">
+            <button onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? <X size={28} /> : <Menu size={28} />}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Dropdown Menu */}
+      {isOpen && (
+        <div className="md:hidden bg-white shadow-md">
+          <div className="px-4 pt-2 pb-4 space-y-2">
+            {navLinks.map((link) => (
+              <Link
+                key={link.name}
+                to={link.to}
+                className="block text-white hover:text-indigo-600 transition"
+                onClick={() => setIsOpen(false)}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+}
