913d788dac220113b540b7d82d5c3ea0ee39e0c8
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-09-21 17:49:40 +0600

    demo caurosal add

---
+++ src/components/Demo.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import banner1 from "../assets/one.jpg";
+import banner2 from "../assets/two.jpg";
+import banner3 from "../assets/three.jpg";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+
+const Demo = () => {
+  return (
+    <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false}>
+      <div>
+        <img src={banner1} />
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img src={banner2} />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img src={banner3} />
+        <p className="legend">Legend 3</p>
+      </div>
+    </Carousel>
+  );
+};
+
+export default Demo;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Banner";
+import Demo from "../../components/Demo";
 
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <Demo/>
        </div>
   );
 };
