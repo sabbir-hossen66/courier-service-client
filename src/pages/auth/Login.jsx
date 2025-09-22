4f79a48ff082f1309e92951b4507c760af8d691e
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-09-22 23:25:11 +0600

    login page create

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import { Eye, EyeOff, LogIn } from "lucide-react";
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
   return(
-    <div>
-      <h2>hello login</h2>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8">
+      <form className="space-y-5">
+          {/* Email */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Email Address
+            </label>
+            <input
+              type="email"
+              placeholder="you@example.com"
+              className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-green-500 focus:border-green-500 outline-none transition"
+              required
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="********"
+                className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-green-500 focus:border-green-500 outline-none transition"
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-3 flex items-center text-gray-400 hover:text-gray-600"
+              >
+                {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+              </button>
+            </div>
+          </div>
+
+          {/* Remember + Forgot */}
+          <div className="flex items-center justify-between text-sm">
+            <label className="flex items-center gap-2">
+              <input type="checkbox" className="rounded bg-green-600" />
+              <span className="text-gray-600">Remember me</span>
+            </label>
+            <a href="#" className="text-green-600 hover:underline">
+              Forgot password?
+            </a>
+          </div>
+
+          {/* Login Button */}
+          <button
+            type="submit"
+            className="w-full flex items-center justify-center gap-2 bg-primary text-white py-2 rounded-xl font-medium text-hover transition shadow-md cursor-pointer"
+          >
+            <LogIn size={18} />
+            Login
+          </button>
+        </form>
+</div>
     </div>
   )
 };
