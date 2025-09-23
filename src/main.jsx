78901b65862377563c56d616c4eed421f89462b2
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-09-23 23:18:52 +0600

    setup react context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { router } from './routes/Router.jsx'
-import { RouterProvider } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { router } from "./routes/Router.jsx";
+import { RouterProvider } from "react-router";
+import AuthProvider from "./contexts/authContext/AuthProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className='font-urbanist'>
-    <RouterProvider router={router} />,
+    <div className="font-urbanist">
+      <AuthProvider>
+        <RouterProvider router={router} />,
+      </AuthProvider>
     </div>
-  </StrictMode>,
-)
+  </StrictMode>
+);
